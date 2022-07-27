e582f8571ba6bebba0f501d7179869a9470b7135
chetna agarwal <82748928+chetnaag95@users.noreply.github.com>
2022-07-27 14:29:21 +0530

    Initial project setup, header and footer layout

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (<div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className="mb-4 text-2xl font-light">A React.js app to search GitHub profiles and see profile details. This app uses the Context API along with the useContext and useReducer hooks for state management and is part of the <strong>React Front To Back</strong> Udemy course from <strong><a href="https://traversymedia.com">Brad Traversy</a></strong>.</p>
+        <p className="mb-4 text-2xl font-light">I have used <strong>TailwindCSS</strong> &amp; <strong>DaisyUI</strong> to make it look even better and user-friendly.</p>
+        <p className="text-lg text-gray-600">Version 1.0.0</p>
+    </div>)
+}
+
+export default About;
